ce9e7be48db2ea0fff38d89402270ac2fb4968ac
Vitaly Koiro <yuutacn@gmail.com>
2025-05-12 16:01:09 +0300

    feat: temp page

--- pages/react.vue
+++ pages/react.vue
@@ -1,5 +1,14 @@
 <script setup></script>
 
 <template>
-  <div>process</div>
+  <div>
+    <SectionsDefault
+      class="section_padding_top section_padding_bottom"
+      :title="$t('common.temp')"
+    >
+      <AppButton :to="$t('pages.home.common.link')" icon="fi-rr-bed">{{
+        $t('common.back_home')
+      }}</AppButton>
+    </SectionsDefault>
+  </div>
 </template>
